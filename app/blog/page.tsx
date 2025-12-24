220807ca33f759500d96defa232c0c22cb38537c
anirudh7065 <anirudhraj02@gmail.com>
2025-12-24 21:45:32 +0530

    add loader in app and blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -19,40 +19,46 @@ const Blog = async () => {
         trips &#41;{" "}
       </h3>
 
-      <section className="mx-auto my-10 flex flex-col w-[80%] justify-center gap-5 px-4 py-2 ">
-        {blogs.map((item, index) => (
-          <article
-            className="relative mb-6  rounded-3xl p-0.5"
-            key={index}
-          >
-            <Link
-              href={{
-                pathname: "/content",
-                query: {
-                  data: "blogPage",
-                  id: index,
-                },
-              }}
-            >
+      {
+        (blogs.length === 0) ? (
+          <h1 className="text-3xl font-bold text-violet-600">No Blogs Found</h1>
+        ) : (
+          <section className="mx-auto my-10 flex flex-col w-[80%] justify-center gap-5 px-4 py-2 ">
+            {blogs.map((item, index) => (
+              <article
+                className="relative mb-6  rounded-3xl p-0.5"
+                key={index}
+              >
+                <Link
+                  href={{
+                    pathname: "/content",
+                    query: {
+                      data: "blogPage",
+                      id: index,
+                    },
+                  }}
+                >
 
 
-              <div className="relative z-10 flex flex-col justify-items-center gap-3 rounded-4xl border-2 border-violet-400 p-8 ">
-                <h1 className="my-2 flex items-center text-lg font-bold text-violet-500 sm:text-4xl">
-                  {item.title}
-                </h1>
-                <div className="line-clamp-4 text-md wrap-break-word text-ellipsis dark:text-violet-200">
-                  <Markdown
-                  >
-                    {item.content}
-                  </Markdown>
-                </div>
+                  <div className="relative z-10 flex flex-col justify-items-center gap-3 rounded-4xl border-2 border-violet-400 p-8 ">
+                    <h1 className="my-2 flex items-center text-lg font-bold text-violet-500 sm:text-4xl">
+                      {item.title}
+                    </h1>
+                    <div className="line-clamp-4 text-md wrap-break-word text-ellipsis dark:text-violet-200">
+                      <Markdown
+                      >
+                        {item.content}
+                      </Markdown>
+                    </div>
 
 
-              </div>
-            </Link>
-          </article>
-        ))}
-      </section>
+                  </div>
+                </Link>
+              </article>
+            ))}
+          </section>
+        )
+      }
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Markdown from "markdown-to-jsx/react";
 import { mussorie } from "@/constants/mussorie_images";
 import Image from "next/image";
 import { getData } from "@/lib/getPrismaData";
+import Loader from "@/components/loader";
 // app/blog/layout.tsx
 export const dynamic = "force-dynamic";
 
@@ -53,54 +54,60 @@ export default async function Home() {
 
       </section>
 
-      <section className="flex lg:w-[80vw] max-lg:w-[90vw] mx-auto gap-10  text-left font-bold max-sm:my-10 max-sm:flex-col md:h-75 items-center justify-center ">
-        <article className="guide flex h-auto flex-col max-sm:px-4 md:w-[47%]">
-          <h1 className="text-2xl text-violet-700 ">Guide</h1>
-          <h3 className="my-2 text-violet-500 line-clamp-2">{guide[0].title}</h3>
-          <div className="my-2 overflow-hidden dark:text-violet-200 line-clamp-3 ">
-            <Markdown
-            >
-              {guide[0].content}
-            </Markdown>
-          </div>
+      {
+        (blogs.length > 0 && guide.length > 0) ? (
+
+          <section className="flex lg:w-[80vw] max-lg:w-[90vw] mx-auto gap-10  text-left font-bold max-sm:my-10 max-sm:flex-col md:h-75 items-center justify-center ">
+            <article className="guide flex h-auto flex-col max-sm:px-4 md:w-[47%]">
+              <h1 className="text-2xl text-violet-700 ">Guide</h1>
+              <h3 className="my-2 text-violet-500 line-clamp-2">{guide[0].title}</h3>
+              <div className="my-2 overflow-hidden dark:text-violet-200 line-clamp-3 ">
+                <Markdown
+                >
+                  {guide[0].content}
+                </Markdown>
+              </div>
 
 
-          <Link
-            href={{
-              pathname: "/content",
-              query: {
-                data: "guidepage",
-                id: 0,
-              },
-            }}
-            className="mx-auto my-2 block rounded-full bg-violet-600 px-4 py-2 text-xs text-violet-100"
-          >
-            Read more
-          </Link>
-        </article>
-        <article className="blog flex flex-col items-start max-sm:px-4 md:w-[47%]">
-          <h1 className="text-2xl text-violet-600">Blogs</h1>
-          <h3 className="my-2 text-violet-500 line-clamp-2">{blogs[0].title}</h3>
-          <div className="my-2 line-clamp-3 overflow-hidden dark:text-violet-200">
-            <Markdown
-            >
-              {blogs[0].content}
-            </Markdown>
-          </div>
-          <Link
-            href={{
-              pathname: "/content",
-              query: {
-                data: "blogpage",
-                id: 0,
-              },
-            }}
-            className="mx-auto my-2 block rounded-full bg-violet-600 px-4 py-2 text-xs text-violet-100"
-          >
-            Read more
-          </Link>
-        </article>
-      </section>
+              <Link
+                href={{
+                  pathname: "/content",
+                  query: {
+                    data: "guidepage",
+                    id: 0,
+                  },
+                }}
+                className="mx-auto my-2 block rounded-full bg-violet-600 px-4 py-2 text-xs text-violet-100"
+              >
+                Read more
+              </Link>
+            </article>
+            <article className="blog flex flex-col items-start max-sm:px-4 md:w-[47%]">
+              <h1 className="text-2xl text-violet-600">Blogs</h1>
+              <h3 className="my-2 text-violet-500 line-clamp-2">{blogs[0].title}</h3>
+              <div className="my-2 line-clamp-3 overflow-hidden dark:text-violet-200">
+                <Markdown
+                >
+                  {blogs[0].content}
+                </Markdown>
+              </div>
+              <Link
+                href={{
+                  pathname: "/content",
+                  query: {
+                    data: "blogpage",
+                    id: 0,
+                  },
+                }}
+                className="mx-auto my-2 block rounded-full bg-violet-600 px-4 py-2 text-xs text-violet-100"
+              >
+                Read more
+              </Link>
+            </article>
+          </section> ): (
+          <Loader />
+        )
+      }
     </main>
   );
 }
